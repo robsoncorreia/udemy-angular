6ebc178855063ea7d020064fb441bbe0bd7894ec
robsoncorreia <robson.correia.bd@gmail.com>
2018-07-12 23:11:58 -0300

    aula finalizada.

--- src/app/restaurant-detail/shopping-cart/cart-item.model.ts
+++ src/app/restaurant-detail/shopping-cart/cart-item.model.ts
@@ -1,3 +1,8 @@
-export interface CartItem{
-    
+import { MenuItem } from '../menu-item/menu-item.model';
+
+export class CartItem {
+  constructor(public menuItem: MenuItem, public quantity = 1) {}
+  value(): number {
+    return this.menuItem.price * this.quantity;
+  }
 }
--- src/app/restaurant-detail/shopping-cart/shopping-cart.service.ts
+++ src/app/restaurant-detail/shopping-cart/shopping-cart.service.ts
@@ -1,22 +1,33 @@
 import { Injectable } from '@angular/core';
+import { CartItem } from './cart-item.model';
+import { MenuItem } from '../menu-item/menu-item.model';
 
 @Injectable()
 export class ShoppingCartService {
+  itens: CartItem[] = [];
 
-  itens: any[];
-
-  constructor() { }
+  constructor() {}
 
   clear() {
-
+    this.itens = [];
   }
 
-  addItem (item: any){
-
+  addItem(item: MenuItem) {
+    let foundItem = this.itens.find(mItem => mItem.menuItem.id === item.id);
+    if (foundItem) {
+      foundItem.quantity += 1;
+    } else {
+      this.itens.push(new CartItem(item));
+    }
   }
-  
+
+  removeItem(item: CartItem) {
+    this.itens.splice(this.itens.indexOf(item), 1);
+  }
+
   total(): number {
-    return 0;
+    return this.itens
+      .map(item => item.value())
+      .reduce((prev, value) => prev + value, 0);
   }
-
 }
